bea93fdc8e8bb7cdb7672f7f0ff08b425bca863f
Diego <diegoojedagarcia@gmail.com>
2014-01-16 11:07:35 +0100

    asdfv

--- iGenda/IGEPersonasTableViewController.m
+++ iGenda/IGEPersonasTableViewController.m
@@ -21,7 +21,7 @@
     IGEAddContactViewController *source = [segue sourceViewController];
     IGEContacto *item = source.contactItem;
     if (item != nil){
-        [self.toDoItems addObject:item];
+        [self.contactos addObject:item];
         [self.tableView reloadData];
     }
 }
@@ -58,14 +58,14 @@
 {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [self.contactos count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
