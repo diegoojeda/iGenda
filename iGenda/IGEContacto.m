4d2ffac27b2d50f7bc8dcb59eac1438bff462047
Diego <diegoojedagarcia@gmail.com>
2014-01-16 10:50:31 +0100

    Clase contacto

---
+++ iGenda/IGEContacto.m
@@ -0,0 +1,20 @@
+//
+//  IGEContacto.m
+//  iGenda
+//
+//  Created by Máster INFTEL 11 on 16/01/14.
+//  Copyright (c) 2014 UMA. All rights reserved.
+//
+
+#import "IGEContacto.h"
+
+@implementation IGEContacto
+
+@synthesize nombre;
+@synthesize apellido1;
+@synthesize apellido2;
+@synthesize email;
+@synthesize grupo;
+@synthesize telefono;
+
+@end
